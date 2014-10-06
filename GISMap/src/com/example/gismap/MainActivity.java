c42d862af232ee310ce242405a7c81730c06602a
mfirk851 <mfirk851@SCIA203-WD-06.uwsp.edu>
2014-10-06 17:10:30 -0500

    initial commit

--- GISMap/src/com/example/gismap/MainActivity.java
+++ GISMap/src/com/example/gismap/MainActivity.java
@@ -1,36 +1,53 @@
-package com.example.gismap;
-
-import android.support.v7.app.ActionBarActivity;
-import android.os.Bundle;
-import android.view.Menu;
-import android.view.MenuItem;
-
-
-public class MainActivity extends ActionBarActivity {
-
-    @Override
-    protected void onCreate(Bundle savedInstanceState) {
-        super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
-    }
-
-
-    @Override
-    public boolean onCreateOptionsMenu(Menu menu) {
-        // Inflate the menu; this adds items to the action bar if it is present.
-        getMenuInflater().inflate(R.menu.main, menu);
-        return true;
-    }
-
-    @Override
-    public boolean onOptionsItemSelected(MenuItem item) {
-        // Handle action bar item clicks here. The action bar will
-        // automatically handle clicks on the Home/Up button, so long
-        // as you specify a parent activity in AndroidManifest.xml.
-        int id = item.getItemId();
-        if (id == R.id.action_settings) {
-            return true;
-        }
-        return super.onOptionsItemSelected(item);
-    }
+package com.example.gismap;  
+  
+import java.util.ArrayList;  
+import java.util.Arrays;  
+  
+import android.app.Activity;  
+import android.content.Intent;
+import android.os.Bundle;  
+import android.view.View;
+import android.widget.AdapterView;
+import android.widget.AdapterView.OnItemClickListener;
+import android.widget.ArrayAdapter;  
+import android.widget.ListView;  
+  
+public class MainActivity extends Activity {  
+    
+  private ListView mainListView ;  
+  private ArrayAdapter<String> listAdapter ;  
+    
+  /** Called when the activity is first created. */  
+  @Override  
+  public void onCreate(Bundle savedInstanceState) {  
+    super.onCreate(savedInstanceState);  
+    setContentView(R.layout.activity_main);  
+      
+    // Find the ListView resource.   
+    mainListView = (ListView) findViewById( R.id.mainListView );  
+  
+    // Create and populate a List of waterbodies.  
+    String[] waterbodies = new String[] { "Stevens Point Flowage", "Wisconsin River", "Little Plover River", 
+                                      "Mississippi River", "Lake Dubay"};    
+    ArrayList<String> waterbodyList = new ArrayList<String>();  
+    waterbodyList.addAll( Arrays.asList(waterbodies) );  
+      
+    // Create ArrayAdapter using the planet list.  
+    listAdapter = new ArrayAdapter<String>(this, R.layout.list_item, waterbodyList);  
+        
+      
+    // Set the ArrayAdapter as the ListView's adapter.  
+    mainListView.setAdapter( listAdapter );  
+    
+    mainListView.setOnItemClickListener(new OnItemClickListener(){
+    	public void onItemClick(AdapterView<?> parent, View view, int position, long id){
+    		switch(position)
+    		{
+    		case 0: Intent newActivity = new Intent(MainActivity.this, StevensPointFlowage.class);
+    		startActivity(newActivity);
+    		break;
+    		}
+    	}
+    });
+  }  
 }
