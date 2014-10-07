15b3fc0f9b259d4d0ee5907a7078194e0fd6109d
mfirk851 <mfirk851@uwsp.edu>
2014-10-06 22:06:43 -0500

    aded map and geolocation functionality

--- GISMap/src/com/example/gismap/StevensPointFlowage.java
+++ GISMap/src/com/example/gismap/StevensPointFlowage.java
@@ -1,16 +1,49 @@
 package com.example.gismap;
 
-import android.support.v7.app.ActionBarActivity;
+import com.esri.android.map.LocationService;
+import com.esri.android.map.MapView;
+import com.esri.android.map.ags.ArcGISTiledMapServiceLayer;
+import com.esri.android.map.event.OnStatusChangedListener;
+
+import android.app.Activity;
 import android.os.Bundle;
 import android.view.Menu;
 import android.view.MenuItem;
 
-public class StevensPointFlowage extends ActionBarActivity {
-
+public class StevensPointFlowage extends Activity {
+	
+	MapView mapView;
+	
 	@Override
 	protected void onCreate(Bundle savedInstanceState) {
 		super.onCreate(savedInstanceState);
 		setContentView(R.layout.activity_stevens_point_flowage);
+		
+		mapView = (MapView)findViewById(R.id.map);
+		
+		mapView.addLayer(new ArcGISTiledMapServiceLayer(""+
+		"http://services.arcgisonline.com/ArcGIS/rest/services/World_Street_Map/MapServer"));
+		
+		mapView.setOnStatusChangedListener(
+				new OnStatusChangedListener(){
+					public void onStatusChanged(Object source, STATUS status){
+						if(source == mapView && status == STATUS.INITIALIZED){
+							LocationService ls = mapView.getLocationService();
+							ls.setAutoPan(true);
+							ls.start();
+						}
+					}
+				});
+	}
+	
+	protected void onPause(){
+		super.onPause();
+		mapView.pause();
+	}
+	
+	protected void onResume(){
+		super.onResume();
+		mapView.unpause();
 	}
 
 	@Override
